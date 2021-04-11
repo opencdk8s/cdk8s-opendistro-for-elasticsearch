b68b29f4dec8fc7b5d0c7a4609a7a99f9e9fcf87
Hunter Thompson <aatman@auroville.org.in>
2021-04-11 23:02:19 +0530

    chore: add mergify and upgrade projen

--- .projenrc.js
+++ .projenrc.js
@@ -31,7 +31,7 @@ const project = new ConstructLibraryCdk8s({
   codeCov: true,
   clobber: false,
   readme: true,
-  mergify: false,
+  mergify: true,
 });
 
 const common_exclude = ['cdk.out', 'package.json', 'yarn-error.log', 'coverage', '.DS_Store', '.idea', '.vs_code'];
